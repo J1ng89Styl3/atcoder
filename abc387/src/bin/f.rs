6c7d2fb4edd754f2c3703419ccf1aed0c6e80dd1
J1ng89Styl3 <jingyue8999xe@gmail.com>
2025-01-16 01:06:29 +0900

    2025/1/16

--- abc387/src/bin/f.rs
+++ abc387/src/bin/f.rs
@@ -1,68 +1,31 @@
+use std::iter::FromIterator;
 #[warn(unused_imports)]
 use std::usize;
 use ascii::Chars;
+use im_rc::HashSet;
 use indexing::container_traits::FixedLength;
 use num::abs;
 use proconio::input;
 use ac_library::*;
+use web_sys::js_sys::Math::max;
 use std::collections::VecDeque;
 use std::cmp;
+use ac_library::Dsu;
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
-fn solve(n: usize, xh: Vec<(usize, usize)>) -> f64{
-    let mut max_ans:f64 = 10_f64.powi(9);
-    let mut min_ans:f64 = 0.0;
-    // 0の実験
-    let mut last: f64 = -f64::MAX;
-    let mut bool = true;
-    for (x, h) in &xh{
-        let this = (*h as f64)/(*x as f64);
-        // 高さが足りなかった
-        if this <= last{
-            bool = false;
-            break;
-        }
-        last = this
-    }
-    if bool{
-       return -1.00;
-    }
-    // ここからが正しい答え
-    while max_ans - min_ans > 10_f64.powi(-10){
-        let this_ans:f64 = (max_ans + min_ans)/2.0;
-        let mut last:(f64, f64) = (0.0, 0.0);
-        let mut bool = true;
-        let mut count: usize = 0;
-
-        for (x, h) in &xh{
-            if count == 0{
-                count += 1;
-                last = (*x as f64, *h as f64);
-                continue;
-            }
-            if (this_ans - *h as f64)*last.0 >= (this_ans - last.1 as f64)*(*x as f64){
-                min_ans = this_ans;
-                bool = false;
-                break;
-
-            }
-            last = (*x as f64, *h as f64);
-        }
-        if bool{
-            max_ans = this_ans;
-        }
-    }
-    return max_ans;
-}
+"""
+一つずつ拡張していく
+"""
 
 fn main(){
     input!{
         n: usize,
-        xh: [(usize, usize); n],
-    }
-    let ans:f64 = solve(n, xh.clone());
-    if ans < 0.0{
-        println!("-1");
-    }else{
-        println!("{}", ans);
+        m: usize,
+        k: usize,
+        mut edge: [(usize, usize, usize); m],
+        a: [usize; k],
+        b: [usize; k]
     }
 }
--- cheatsheet.md
+++ cheatsheet.md
@@ -8,6 +8,7 @@ input! {
 let matrix = vec![vec![0; cols]; rows];
 
 
+## 変換
 ## 数を一桁ずつ分解して数列に変換
 let digits: Vec<u32> = number
     .to_string() // 数字を文字列に変換
@@ -15,8 +16,30 @@ let digits: Vec<u32> = number
     .map(|c| c.to_digit(10).unwrap()) // 文字を数字に変換
     .collect();   // Vecに収集
 
+## 文字を文字列に変換
+
 
 ## 構造体
 let pos: Vec<usize> = vec![];
 ###　 前後の処理
 let pos: VecDeque<(usize)> = VecDeque::new();　
+
+## 辞書型の初期化
+let mut map: HashMap<&str, i32> = HashMap::new();
+
+## 集合型
+let mut set = HashSet::new();  // Set
+let mut heap = BinaryHeap::new(); //優先度付きキュー(大きい方から取り出す)
+let mut min_heap: BinaryHeap<Reverse<usize>> = BinaryHeap::new(); //優先度付きキュー(小さい方から取り出す)
+
+## 複数値でのソート
+edges.sort_by_key(|&(_, _, w)| w);
+
+
+## ライブラリ関連
+**union find**
+https://github.com/atcoder/ac-library/blob/master/document_ja/dsu.md
+let mut dsu = Dsu::new(n);
+dsu.merge(u, v); # 結合
+dsu.same(u, v); # 判定
+
