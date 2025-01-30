6b849ff107af49e45919d828773efbb1c470fee4
J1ng89Styl3 <jingyue8999xe@gmail.com>
2025-01-30 13:18:06 +0900

    aa

--- abc387/src/bin/a.rs
+++ abc387/src/bin/a.rs
@@ -3,6 +3,7 @@ use std::iter::FromIterator;
 use std::usize;
 use ascii::Chars;
 use bitvec::vec;
+use im_rc::hashmap::Values;
 use im_rc::{hashmap, HashSet};
 use indexing::container_traits::FixedLength;
 use num::abs;
@@ -22,25 +23,52 @@ use itertools::{Itertools, Update};
 use num::Integer;
 use std::process::exit;
 use ac_library::ModInt998244353 as Mint;
+use std::collections::BTreeMap;
+
 
 fn main() {
     input! {
-        a:isize,
-        b:isize,
-        c:isize,
-        d:isize,
+        n:usize,
+        q:usize,
     }
-    let mut answer = 0;
-    let x_diff = a.abs_diff(c);
-    let y_diff = b.abs_diff(d);
-    answer += (x_diff / 4) * y_diff * 4;
-    let x_black = [3, 3, 1, 1];
-    let point_black = [[2, 1], [1, 2], [0, 1], [1, 0]];
-    for i in 0..x_diff % 4 {
-        answer += x_black[(a + 1e9 as isize + i as isize) as usize % 4] * (y_diff / 2);
-        answer += point_black[(a + 1e9 as isize + i as isize) as usize % 4]
-            [(b + 1e9 as isize) as usize % 2]
-            * (y_diff % 2);
+
+    let mut lefts = BTreeMap::new();
+    for i in 0..=n + 1 {
+        lefts.insert(i, i);
     }
-    println!("{answer}");
+    let mut cnts = vec![1; n + 2];
+    for _ in 0..q {
+        input! {
+            t:usize,
+        }
+
+        if t == 1 {
+            input! {
+                x:usize,
+                c_nxt:usize,
+            }
+
+            let (&l_x, &c_prv) = lefts.range(..=x).last().unwrap();
+            let (&r_x, &c_r) = lefts.range(x + 1..).next().unwrap();
+            let (&_, &c_l_l) = lefts.range(..l_x).last().unwrap();
+
+            cnts[c_prv] -= r_x - l_x;
+            *lefts.get_mut(&l_x).unwrap() = c_nxt;
+            cnts[c_nxt] += r_x - l_x;
+
+            if c_nxt == c_l_l {
+                lefts.remove(&l_x);
+            }
+            if c_nxt == c_r {
+                lefts.remove(&r_x);
+            }
+        } else {
+            input! {
+                c:usize,
+            }
+
+            let ans = cnts[c];
+            println!("{}", ans);
+        }
+    }
 }
--- cheatsheet.md
+++ cheatsheet.md
@@ -25,12 +25,12 @@ let pos: Vec<usize> = vec![];
 let pos: VecDeque<(usize)> = VecDeque::new();　
 
 ## 辞書型の初期化
-let mut map: HashMap<&str, i32> = HashMap::new();
+let mut map: HashMap = HashMap::new();
+*dict_b.entry(*v).or_insert(0) += 1; //辞書型の操作
+
 
 ## 集合型
 let mut set = HashSet::new();  // Set
-let mut map = HashMap::new(); // Dict
-*dict_b.entry(*v).or_insert(0) += 1; //辞書型の操作
 dict_b.get(&v).copied().unwrap_or(0);
 
 let mut heap = BinaryHeap::new(); //優先度付きキュー(大きい方から取り出す)
@@ -41,8 +41,5 @@ edges.sort_by_key(|&(_, _, w)| w);
 
 ## 出力に関するもの
 let result = numbers
-        .iter() // 各要素を順に処理
-        .map(|n| n.to_string()) // 数値を文字列に変換
-        .collect::<Vec<_>>() // Vec<String> に収集
-        .join(" "); // 空白区切りで結合
+        .iter().map(|n| n.to_string()).collect::<Vec<_>>().join(" ");
 
